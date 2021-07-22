2eb73928365730ec7396e510b624fea7034466f0
princeinarabic <emirchyngyz27@gmail.com>
2021-07-22 23:54:03 +0600

    Font style buttons are working properly

--- src/Editor.cs
+++ src/Editor.cs
@@ -11,17 +11,17 @@ public class Window : Form {
     ToolStripButton boldBTN, italicBTN, underlineBTN;  // BTN - button
     ToolStripButton alignLeftBTN, alignCenterBTN, alignRightBTN, bulletListBTN;
     ToolStripSeparator bar;
-    Font font, heading1Font, heading2Font, heading3Font;
 
     ToolStripDropDownButton headingsBTN;
     ToolStripDropDown dropDown;
     ToolStripButton heading1BTN, heading2BTN, heading3BTN;
+    Font font, heading1Font, heading2Font, heading3Font;
 
     public Window() {
         ClientSize = new Size(500, 500);
         StartPosition = FormStartPosition.CenterScreen;
 
-        font = new Font("Arial", 11, FontStyle.Regular);
+        initFonts();
 
         textBox = new RichTextBox();
         textBox.Font = font;
@@ -29,7 +29,7 @@ public class Window : Form {
         textBox.Dock = DockStyle.Fill;
         textBox.ShowSelectionMargin = true;
         textBox.SelectionRightIndent = 7;
-        textBox.BackColor = Color.OldLace;  // Moccasin  Wheat  OldLace MintCream
+        textBox.BackColor = Color.OldLace;  
         Controls.Add(textBox);         
 
         /* ToolStripMenuItem(String, Image, EventHandler)	
@@ -40,9 +40,9 @@ public class Window : Form {
             new ToolStripMenuItem("Quit", null, onQuit),
             new ToolStripMenuItem("Save", null, onSave)
         };
-        
+                
         ToolStripMenuItem[] topItems = {
-            new ToolStripMenuItem("File", null, fileItems)
+            new ToolStripMenuItem("File", null, fileItems),
         };
         
         MenuStrip menuStrip = new MenuStrip();
@@ -54,7 +54,7 @@ public class Window : Form {
         this.toolStrip = new ToolStrip();
         toolStrip.GripStyle = ToolStripGripStyle.Hidden;  // hides the "grip" which are three dots you use to move the buttons
         toolStrip.BackColor = Color.LightGreen;  //MediumPurple
-
+        
         initIcons();
         buttonActions();
 
@@ -63,6 +63,54 @@ public class Window : Form {
 
         menuStrip.Dock = DockStyle.Top;  // adding it after toolStrip to make the menu on top of buttons
         Controls.Add(menuStrip);         // because whatever is added last goes on top.
+    }
+
+    void initFonts() {
+        font = new Font("Arial", 11, FontStyle.Regular);
+        heading1Font = new Font("Arial", 20, FontStyle.Bold);
+        heading2Font = new Font("Arial", 18, FontStyle.Bold);
+        heading3Font = new Font("Arial", 16, FontStyle.Bold);
+    }
+
+    FontStyle newBoldFont;  // build error when put within ToggleFontStyle()
+    void boldBTN_Click(object sender, EventArgs e) {
+        if (textBox.SelectionFont != null) {
+            Font currentFont = textBox.SelectionFont;
+
+            if (textBox.SelectionFont.Bold == true) 
+                newBoldFont = FontStyle.Regular;
+            else    
+                newBoldFont = FontStyle.Bold;
+
+            textBox.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newBoldFont);
+        }
+    }
+
+    FontStyle newItalicFont;
+    void italicBTN_Click(object sender, EventArgs e) {
+        if (textBox.SelectionFont != null) {
+            Font currentFont = textBox.SelectionFont;
+
+            if (textBox.SelectionFont.Italic == true) 
+                newItalicFont = FontStyle.Regular;
+            else    
+                newItalicFont = FontStyle.Italic;
+            textBox.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newItalicFont);
+        }
+    }
+
+    FontStyle newUnderlineFont;
+    void underlineBTN_Click(object sender, EventArgs e) {
+        if (textBox.SelectionFont != null) {
+            Font currentFont = textBox.SelectionFont;
+
+            if (textBox.SelectionFont.Underline == true) 
+                newUnderlineFont = FontStyle.Regular;
+            else    
+                newUnderlineFont = FontStyle.Underline;
+
+            textBox.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newUnderlineFont);
+        }
     }
 
     void bulletButtonChecked(object sender, EventArgs e) {
@@ -81,57 +129,10 @@ public class Window : Form {
             textBox.SelectionAlignment = HorizontalAlignment.Left;
     }
 
-    FontStyle newFontStyle;  // build error when put within ToggleFont()
-    void ToggleFontStyle(string t) {
-        if (textBox.SelectionFont != null) {
-            Font currentFont = textBox.SelectionFont;
-        
-            if (textBox.SelectionFont.Bold == true) {
-                boldBTN.Checked = false;
-                newFontStyle = FontStyle.Regular;
-            }
-            else if (textBox.SelectionFont.Italic == true) {
-                newFontStyle = FontStyle.Regular;
-            }
-            else if (textBox.SelectionFont.Underline == true) {
-                newFontStyle = FontStyle.Regular;
-            }
-            else {
-                if (t == "Bold") {
-                    newFontStyle = FontStyle.Bold;
-                }
-                else if (t == "Italic") {
-                    newFontStyle = FontStyle.Italic;
-                }
-                else if (t == "Underline") {
-                    newFontStyle = FontStyle.Underline;  
-                }
-            }
-
-            textBox.SelectionFont = new Font(
-                "Arial", 
-                11, 
-                newFontStyle
-            );
-        }
-    }
-
-    // NEEDS TO BE FIXED, DOESN'T WORK PROPERLY. 
-    void styleButtonChecked(object sender, EventArgs e) {
-        if (boldBTN.Checked) 
-            ToggleFontStyle("Bold");
-        else if (italicBTN.Checked)
-            ToggleFontStyle("Italic");
-        else if (underlineBTN.Checked)
-            ToggleFontStyle("Underline");
-        // else
-        //     textBox.Font = font;
-    }
-
     void buttonActions() {
-        boldBTN.Click += styleButtonChecked;
-        italicBTN.Click += styleButtonChecked;
-        underlineBTN.Click += styleButtonChecked;
+        boldBTN.Click += boldBTN_Click;
+        italicBTN.Click += italicBTN_Click;
+        underlineBTN.Click += underlineBTN_Click;
 
         alignRightBTN.Click += alignmentButtonChecked;
         alignCenterBTN.Click += alignmentButtonChecked;
@@ -204,7 +205,6 @@ public class Window : Form {
     }                                                       
 
     void savingFile(string filename) {
-        // Save the contents of the RichTextBox into the file.
         textBox.SaveFile(filename, RichTextBoxStreamType.PlainText);
         MessageBox.Show("Saved Successfully", "File address: " + 
                         filename, MessageBoxButtons.OK, MessageBoxIcon.Information);
